e97cc4de1f717ae61fa405ff110800bd5120951e
Satyam <satyamsingh26002@gmail.com>
2026-03-01 13:30:48 +0530

    updated

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -48,7 +48,7 @@ try {
 
     const user=await User.findOne({email});
     if(!user){
-        return res.status(400).json({message:"Invalid credentials !"});
+        return res.status(400).json({message:"Invalid Credentials !"});
     }
 
     const isMatch= await bcrypt.compare(password,user.password)

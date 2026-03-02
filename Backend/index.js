807be9e716dc990abcbb5ec1734cfbb75a4a574d
Satyam <satyamsingh26002@gmail.com>
2026-03-02 20:12:06 +0530

    index

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './config/db.js';
 import userRoute from './router/user.route.js';
+import cors from 'cookie-parser';
 const app=express();
 dotenv.config();
 const port=process.env.PORT || 5000;
@@ -10,6 +11,11 @@ connectDb();
 app.get('/',(req,res)=>{
     res.send("Backend working ! ")
 })
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
 // route middleware
 app.use(express.json())

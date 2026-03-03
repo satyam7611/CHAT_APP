daafb2faa96a765da06942ee0d7af52d8feae12a
Satyam <satyamsingh26002@gmail.com>
2026-03-03 17:54:47 +0530

    app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,28 @@
+import SignUp from "./components/SignUp"
 import Left from "./Home/Left/Left"
 import Logout from "./Home/Left1/Logout"
 import Right from "./Home/right/Right"
+import Login from "./components/Login"
 function App() {
   return (
-    <div className="flex min-h-screen overflow-hidden">
+
+<>
+ {/* <div className="flex min-h-screen overflow-hidden">
       
      <Logout/>
      <Left/>
-     <Right/>
+     <Right/> 
+   
+  
   
    
-    </div>
+    </div> */}
+      <SignUp/>
+     {/* <Login/> */}
+</>
+   
+ 
+   
   )
 }
 

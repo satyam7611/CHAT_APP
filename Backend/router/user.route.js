17c697fa182327b11ac53017712a9df0a12a953b
Satyam <satyamsingh26002@gmail.com>
2026-03-05 20:41:25 +0530

    route

--- Backend/router/user.route.js
+++ Backend/router/user.route.js
@@ -1,11 +1,13 @@
 import User from "../models/user.models.js";
 import express from 'express';
-import { signIn, signUp,logout } from "../controller/user.controller.js";
+import { signIn, signUp,logout ,getUserProfile } from "../controller/user.controller.js";
 
-
+import secureRoute from "../middleware/secureRoute.js";
 const router=express.Router();
 router.post('/signup',signUp);
 router.post('/signin',signIn)
 router.post('/logout',logout);
+
+router.get("/getUserProfile", secureRoute,getUserProfile);
 
 export default router;

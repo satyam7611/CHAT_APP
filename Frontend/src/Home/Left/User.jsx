7b07e8b4ae1117027b7fe1ca160729ac535b9338
Satyam <satyamsingh26002@gmail.com>
2026-03-07 23:16:23 +0530

    user

--- Frontend/src/Home/Left/User.jsx
+++ Frontend/src/Home/Left/User.jsx
@@ -1,9 +1,21 @@
 
+import { useEffect } from "react";
 import Users from "./Users";
+import axios from 'axios';
+import GetAllUser from "../../context/GetAllUser";
 const User = () => {
+const [allUsers,loading]=GetAllUser();
+if(loading){
+  return <p className="text-center">Loading....</p>
+}
   return (
-    <div className="overflow-y-auto" style={{height:"calc(100vh)"}}>
-           <Users name="Satyam" email="satyam@gmail.com" />
+  <div className="overflow-y-auto h-[calc(100vh-120px)]">
+         {
+          allUsers.map((e,index)=>(
+              <Users key={index} name={e.name} id={index}  email={e.email} />
+          ))
+         }
+           {/* <Users name="Satyam" email="satyam@gmail.com" />
            <Users name="Anshu" email="anshu@gmail.com" />
            <Users name="Aniket" email="aniket@gmail.com"/>
            <Users name="Sourya" email="sourya@gmail.com"/>
@@ -11,7 +23,7 @@ const User = () => {
            <Users name="Shivam" email="shivam@gmail.com"/>
            <Users name="Shreyanshu" email="shreyanshu@gmail.com"/>
            <Users name="Himanshu" email="himanshu@gmail.com"/>
-           <Users name="Gaurav" email='gaurav@gmail.com'/>
+           <Users name="Gaurav" email='gaurav@gmail.com'/> */}
     
     </div>
   

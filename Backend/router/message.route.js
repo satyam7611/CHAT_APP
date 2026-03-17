9c15176c1919a1baa9406dee5a003d5847f16066
Satyam <satyamsingh26002@gmail.com>
2026-03-17 21:31:47 +0530

    route

--- Backend/router/message.route.js
+++ Backend/router/message.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { sendMessage,getMessages } from '../controller/message.controller.js';
 import secureRoute from '../middleware/secureRoute.js'
 const router=express.Router();
-
+// below created the post route for the send and get message all
 router.post("/send/:id",secureRoute,sendMessage);
 router.get("/get/:id",secureRoute,getMessages);
 

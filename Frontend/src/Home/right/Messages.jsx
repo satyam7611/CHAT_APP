7d760c205f2a8243b7eb7524ae43ac6c6cbf8dc2
Satyam <satyamsingh26002@gmail.com>
2026-03-18 22:18:45 +0530

    messages

--- Frontend/src/Home/right/Messages.jsx
+++ Frontend/src/Home/right/Messages.jsx
@@ -1,22 +1,25 @@
+import useGetMessage from "../../context/useGetMessage.jsx";
+import Loading from "../../components/Loading.jsx";
 import Message from "./Message";
 const Messages = () => {
+  const { messages, loading } = useGetMessage();
+  // console.log("message from backend",messages);
   return (
     <>
-      {/* <div> */}
+      {loading ? (
+        <Loading></Loading>
+      ) : (
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))
+      )}
       <div className="flex-1 overflow-y-auto p-2">
-       
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+        {!loading && messages.length === 0 && (
+          <div>
+            <p className="text-center mt-[20%] text-2xl">say hi</p>
+          </div>
+        )}
       </div>
     </>
   );

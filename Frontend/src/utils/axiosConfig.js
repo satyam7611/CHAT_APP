1bc1644aef52083768bee32062637aff44d8d2de
Satyam <satyamsingh26002@gmail.com>
2026-05-22 20:42:26 +0530

    updating the filter option

--- Frontend/src/utils/axiosConfig.js
+++ Frontend/src/utils/axiosConfig.js
@@ -8,6 +8,7 @@ const axiosInstance = axios.create({
   baseURL: baseURL,
   withCredentials: true, // Always attach cookies (JWTs) automatically
 });
+console.log("Base url is :",baseURL)
 
 // Example: You could also add interceptors here if needed for global error handling
 // axiosInstance.interceptors.response.use(
